20493e49169820bbd553ae4156c82e3b72c5caee
itsahnafarafat <ahnafarafat.py@gmail.com>
2025-06-03 15:25:29 +0000

    Added views for freelancer and client profile creation

--- core/views.py
+++ core/views.py
@@ -1,33 +1,41 @@
-from rest_framework.views import APIView
+from rest_framework import generics, status
 from rest_framework.response import Response
-from rest_framework import status
-from django.contrib.auth import authenticate
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+from django.contrib.auth import login
+from .serializers import (
+    RegisterSerializer,
+    LoginSerializer,
+    FreelancerProfileSerializer,
+    ClientProfileSerializer
+)
+from .models import FreelancerProfile, ClientProfile
 
-from .serializers import RegisterSerializer, LoginSerializer, FreelancerProfileSerializer
-from .models import FreelancerProfile
+# Registration
+class RegisterView(generics.CreateAPIView):
+    serializer_class = RegisterSerializer
 
-class RegisterView(APIView):
-    permission_classes = [AllowAny]
-
-    def post(self, request):
-        serializer = RegisterSerializer(data=request.data)
-        if serializer.is_valid():
-            user = serializer.save()
-            return Response({"message": "User registered successfully!"}, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
+# Login
 class LoginView(APIView):
-    permission_classes = [AllowAny]
-
     def post(self, request):
         serializer = LoginSerializer(data=request.data)
-        if serializer.is_valid():
-            user = authenticate(
-                username=serializer.validated_data['username'],
-                password=serializer.validated_data['password']
-            )
-            if user:
-                return Response({"message": "Login successful!"}, status=status.HTTP_200_OK)
-            return Response({"error": "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data
+        login(request, user)
+        return Response({"message": "Login successful"}, status=status.HTTP_200_OK)
+    
+# Freelancer profile creation
+class FreelanceProfileView(generics.CreateAPIView):
+    serializer_class = FreelancerProfileSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+# Client profile creation
+class ClientProfileView(generics.CreateAPIView):
+    serializer_class = ClientProfileSerializer
+    permission_classes =[IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
