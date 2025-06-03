915830f8735b49bc98761dfedfdd81cf9d6e734c
itsahnafarafat <ahnafarafat.py@gmail.com>
2025-06-03 15:31:13 +0000

    Added URLs for role-based profile creation

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import RegisterView, LoginView
+from .views import RegisterView, LoginView, FreelanceProfileView, ClientProfileView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
+    path('freelancer/profile/', FreelanceProfileView.as_view(), name='freelancer-profile'),
+    path('client/profile/', ClientProfileView.as_view(), name='client-profile'),
 ]
