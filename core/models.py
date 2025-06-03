6c0aad5fc0edb966018bb84023674645c72d0128
itsahnafarafat <ahnafarafat.py@gmail.com>
2025-06-03 15:03:41 +0000

    Added serializers for freelancer and client profiles

--- core/models.py
+++ core/models.py
@@ -3,8 +3,24 @@ from django.contrib.auth.models import AbstractUser
 from django.conf import settings
 
 class User(AbstractUser):
-    pass
+    ROLE_CHOICES = (
+        ('client', 'Client'),
+        ('freelancer', 'Freelancer'),
+    )
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, null=True, blank=True)
 
+    def __str__(self):
+        return self.username
+    
+class ClientProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    company_name = models.CharField(max_length=100, blank=True)
+    website = models.URLField(blank=True)
+    bio = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.user.username
+    
 class FreelancerProfile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     skills = models.TextField()
--- core/serializers.py
+++ core/serializers.py
@@ -1,13 +1,15 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-from .models import FreelancerProfile
+from django.contrib.auth import authenticate
+from .models import FreelancerProfile, ClientProfile
 
+#User registration serializer
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password']
+        fields = ['id','username', 'email', 'password']
 
     def create(self, validated_data):
         user = User.objects.create_user(
@@ -16,12 +18,30 @@ class RegisterSerializer(serializers.ModelSerializer):
             password=validated_data['password']
         )
         return user
-    
+
+#User Login serializer 
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField()
     password = serializers.CharField()
 
+    def validate(self, data):
+        user = authenticate(**data)
+        if user and user.is_active:
+            return user
+        raise serializers.ValidationError("Invalid credentials")
+
+#Freelance profile serializer
 class FreelancerProfileSerializer(serializers.ModelSerializer):
+    user = serializers.ReadOnlyField(source='user.username')
+
     class Meta:
         model = FreelancerProfile
-        fields = '__all__'
+        fields = ['id', 'user', 'skills', 'experience', 'bio']
+
+#Client profile serializer
+class ClientProfileSerializer(serializers.ModelSerializer):
+    user = serializers.ReadOnlyField(source='user.username')
+
+    class Meta:
+        model = ClientProfile
+        fields = ['id', 'user', 'company_name', 'bio']
