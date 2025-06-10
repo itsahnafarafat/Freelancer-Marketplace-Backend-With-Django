9a507e2fe24880e8d37972cbcda69a5517dbd312
itsahnafarafat <ahnafarafat.py@gmail.com>
2025-06-10 01:43:34 +0000

    Add serializers for JobPost and Application models

--- core/serializers.py
+++ core/serializers.py
@@ -1,47 +1,21 @@
 from rest_framework import serializers
-from django.contrib.auth.models import User
-from django.contrib.auth import authenticate
-from .models import FreelancerProfile, ClientProfile
+from .models import JobPost, Application
+from django.contrib.auth import get_user_model
 
-#User registration serializer
-class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
+User = get_user_model()
 
-    class Meta:
-        model = User
-        fields = ['id','username', 'email', 'password']
-
-    def create(self, validated_data):
-        user = User.objects.create_user(
-            username=validated_data['username'],
-            email=validated_data.get('email'),
-            password=validated_data['password']
-        )
-        return user
-
-#User Login serializer 
-class LoginSerializer(serializers.Serializer):
-    username = serializers.CharField()
-    password = serializers.CharField()
-
-    def validate(self, data):
-        user = authenticate(**data)
-        if user and user.is_active:
-            return user
-        raise serializers.ValidationError("Invalid credentials")
-
-#Freelance profile serializer
-class FreelancerProfileSerializer(serializers.ModelSerializer):
-    user = serializers.ReadOnlyField(source='user.username')
+class JobPostSerializer(serializers.ModelSerializer):
+    client = serializers.StringRelatedField(read_only=True)
 
     class Meta:
-        model = FreelancerProfile
-        fields = ['id', 'user', 'skills', 'experience', 'bio']
+        model = JobPost
+        fields = '__all__'
 
-#Client profile serializer
-class ClientProfileSerializer(serializers.ModelSerializer):
-    user = serializers.ReadOnlyField(source='user.username')
+class ApplicationSerializer(serializers.ModelSerializer):
+    freelancer = serializers.StringRelatedField(read_only=True)
+    job = serializers.StringRelatedField(read_only=True)
 
     class Meta:
-        model = ClientProfile
-        fields = ['id', 'user', 'company_name', 'bio']
+        model = Application
+        fields = '__all__'
+
