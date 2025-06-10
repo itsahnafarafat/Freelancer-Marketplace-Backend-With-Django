9531539b380f5c89befdb74c522a629b13a15dfa
itsahnafarafat <ahnafarafat.py@gmail.com>
2025-06-10 09:18:41 +0000

    Add ownership filtering for job posts and applications

--- core/views.py
+++ core/views.py
@@ -1,14 +1,20 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
-
 from .models import JobPost, Application
 from .serializers import JobPostSerializer, ApplicationSerializer
-from .permissions import IsClient, IsFreelancer, IsOwnerOrReadOnly
+from .permissions import IsClient, IsFreelancer
+from django.db.models import Q
 
 class JobPostViewSet(viewsets.ModelViewSet):
     queryset = JobPost.objects.all()
     serializer_class = JobPostSerializer
     permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.freelancerprofile.role == 'client':
+            return JobPost.objects.filter(client=user)
+        return JobPost.objects.none()
 
     def get_permissions(self):
         if self.action in ['create', 'update', 'partial_update', 'destroy']:
@@ -23,6 +29,12 @@ class ApplicationViewSet(viewsets.ModelViewSet):
     queryset = Application.objects.all()
     serializer_class = ApplicationSerializer
     permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.freelancerprofile.role == 'freelancer':
+            return Application.objects.filter(freelancer=user)
+        return Application.objects.none()
 
     def get_permissions(self):
         if self.action == 'create':
